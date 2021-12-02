13772a7e98063989d17f8cd783d070eb99028e4b
Nathália Barbosa <88548056+nathaliagft@users.noreply.github.com>
2021-12-02 13:52:57 -0300

    Componente relatedListGroupMember
    Componente responsável pela chamada do componente datatableGroupMember  onde consta a tabela com os membros do grupo

---
+++ creditas-sf/force-app/main/default/lwc/relatedListGroupMember/relatedListGroupMember.js
@@ -0,0 +1,18 @@
+import { api, LightningElement } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+export default class RelatedListGroupMember extends NavigationMixin(LightningElement) {
+    @api recordId = '';
+    navigateToTab() {
+       // console.log(this.recordId);
+        this[NavigationMixin.Navigate]({            
+            type: 'standard__navItemPage',
+            attributes: {
+                //Name of any CustomTab. Visualforce tabs, web tabs, Lightning Pages, and Lightning Component tabs
+                apiName: 'DataGroupMember'
+            },
+            state: {
+               c__recordId: this.recordId
+            }
+        });
+    }
+}
