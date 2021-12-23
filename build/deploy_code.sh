93bf3ddd85e3a6f2261780bc3a141572f8b51d98
gabryelss <gabryelss@gmail.com>
2021-12-23 17:21:28 -0300

    CircleCI Branch Estável
    Criação de uma branch de CircleCI com código estável de develop

---
+++ build/deploy_code.sh
@@ -0,0 +1,9 @@
+echo "Changing to SFDX Project Directory"
+cd creditas-sf
+
+#echo "Coverting source to metadata format"
+#sfdx force:source:convert -d deploy_code -r force-app
+
+echo "Executing deploy checkonly mode with tests"
+#sfdx force:mdapi:deploy -u DevHub -d deploy_code/ -w -1 -l RunLocalTests
+sfdx force:source:deploy -u circleci@creditas.com --checkonly --sourcepath force-app --testlevel RunLocalTests
