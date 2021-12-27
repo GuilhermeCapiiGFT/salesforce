228ce28d7fcf0162fab6a19890cd77967401becb
gabryelss <gabryelss@gmail.com>
2021-12-27 12:31:03 -0300

    CICD Test
    Remoção da execução dos scripts de teste para o ambiente test. Ajuste nos objetos para fechar o deploy

--- build/deploy_code.sh
+++ build/deploy_code.sh
@@ -6,4 +6,6 @@ cd creditas-sf
 
 echo "Executing deploy checkonly mode with tests"
 #sfdx force:mdapi:deploy -u DevHub -d deploy_code/ -w -1 -l RunLocalTests
-sfdx force:source:deploy -u circleci@creditas.com --checkonly --sourcepath force-app --testlevel RunLocalTests
+#sfdx force:source:deploy -u circleci@creditas.com --checkonly --sourcepath force-app --testlevel RunLocalTests
+
+sfdx force:source:deploy -u circleci@creditas.com --checkonly --sourcepath force-app
