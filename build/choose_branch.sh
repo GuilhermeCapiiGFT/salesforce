454a2bcc41e198e9229ddb05bbbc73757684f063
gabryelss <gabryelss@gmail.com>
2022-02-15 12:08:58 -0300

    Test target branch variable
    Test target branch variable.

---
+++ build/choose_branch.sh
@@ -0,0 +1,10 @@
+if [[ -n ${CIRCLE_PR_NUMBER} ]]
+then
+curl -L "https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64" \
+      -o jq
+chmod +x jq
+url="https://api.github.com/repos/org/repo/pulls/$CIRCLE_PR_NUMBER?access_token=$GITHUB_TOKEN"
+target_branch=$(
+  curl "$url" | ./jq '.base.ref' | tr -d '"'
+)
+echo "Target Branch: $target_branch";
