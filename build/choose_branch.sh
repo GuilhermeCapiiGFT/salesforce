b56eb82b4bfc455305e5d77e5e17a57aced1eb19
gabryelss <gabryelss@gmail.com>
2022-02-15 14:15:30 -0300

    Update choose_branch.sh
    Adjustment to sh

--- build/choose_branch.sh
+++ build/choose_branch.sh
@@ -1,10 +1,11 @@
 if [[ -n ${CIRCLE_PR_NUMBER} ]]
 then
 curl -L "https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64" \
-      -o jq
+  -o jq
 chmod +x jq
-url="https://api.github.com/repos/org/repo/pulls/$CIRCLE_PR_NUMBER?access_token=$GITHUB_TOKEN"
+url="https://api.github.com/repos/$ORG/$PROJECT/pulls/$CIRCLE_PR_NUMBER?access_token=$GITHUB_TOKEN"
 target_branch=$(
   curl "$url" | ./jq '.base.ref' | tr -d '"'
 )
+fi
 echo "Target Branch: $target_branch";
