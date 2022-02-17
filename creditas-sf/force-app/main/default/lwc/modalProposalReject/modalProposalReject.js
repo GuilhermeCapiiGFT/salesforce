ef56f654614a35bf3b79f0152578d46a7d154db8
npereiragft <gft.npereida@creditas.com>
2022-02-17 13:44:40 -0300

    Add modal to reject proposal
    Modal utilizado na confirmação da rejeição da proposta.

---
+++ creditas-sf/force-app/main/default/lwc/modalProposalReject/modalProposalReject.js
@@ -0,0 +1,39 @@
+import { LightningElement } from 'lwc';
+
+export default class ModalProposalReject extends LightningElement {
+
+    modalHeader = 'Rejeição da proposta';
+    modalBody = 'Deseja mesmo rejeitar esta proposta?';
+    modalBodyReturn = 'Volte a tela de análise e faça uma revisão das rejeições.';
+    contentConfirmation = true;
+    contentSave = false;
+    contentReturn = false;
+    
+
+    handlerAgree(){
+        this.contentConfirmation = false;
+        this.contentReturn = false;  
+        this.contentSave = true;
+    }
+
+    handlerDisagrees(){
+        this.contentConfirmation = false;
+        this.contentSave = false;
+        this.contentReturn = true;    
+    }
+
+    handlerClose(){
+        const selectedEvent = new CustomEvent('closemodal', {
+            bubbles    : true,
+            composed   : true,
+            cancelable : true,
+            detail: {}
+        });
+        this.dispatchEvent(selectedEvent);
+    }
+
+    handlerSave(){
+        this.handlerClose();
+    }
+    
+}
