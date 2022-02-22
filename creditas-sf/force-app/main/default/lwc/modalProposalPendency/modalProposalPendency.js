bbad28ad1984c3864ef76dabe3c1ad24a6ae1391
npereiragft <gft.npereida@creditas.com>
2022-02-22 16:37:59 -0300

    GFT-145 Add modal Pendency

---
+++ creditas-sf/force-app/main/default/lwc/modalProposalPendency/modalProposalPendency.js
@@ -0,0 +1,54 @@
+import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class ModalProposalPendency extends LightningElement {
+
+    modalHeader = 'Pendenciar Proposta';
+    modalBody = 'Ao pendenciar a proposta não será possível editar os campos';
+    
+    contentConfirmation = true;
+    
+    handlerAgree(){
+      this.contentConfirmation = false;
+      this.handlerPendency()
+      this.handlerClose()
+    }
+
+    handlerDisagrees(){
+        this.contentConfirmation = false;
+        this.handlerClose()
+    }
+  
+    handlerPendency(event) {
+      const selectedEvent = new CustomEvent('pending', {
+        bubbles    : true,
+        composed   : true,
+        cancelable : true,
+        detail: {}
+      });
+      this.dispatchEvent(selectedEvent);
+    }
+
+    handlerClose(){
+        const selectedEvent = new CustomEvent('closemodal', {
+            bubbles    : true,
+            composed   : true,
+            cancelable : true,
+            detail: {}
+        });
+        this.dispatchEvent(selectedEvent);
+    }
+
+    handlerSave(){
+        this.handlerClose();
+    }
+  
+    showToast(title, message, variant) {
+      const event = new ShowToastEvent({
+          title: title,
+          message: message,
+          variant: variant
+      });
+      this.dispatchEvent(event);
+    }  
+}
